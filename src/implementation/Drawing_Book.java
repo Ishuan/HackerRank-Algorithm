a122581bef13d053a84e7acfc87018b2e3cc5788
Ishan <ishanagarwal.cs@gmail.com>
2017-11-19 05:44:44 -0500

    Hacker Rank Algo Commit:8

--- src/implementation/Drawing_Book.java
+++ src/implementation/Drawing_Book.java
@@ -4,25 +4,30 @@ import java.util.Scanner;
 
 public class Drawing_Book {
 
-/*    static int solve(int n, int p){
-      int min;
-        int a = (int) Math.ceil((n-p)/2);
-        int b = ((int) Math.floor((p-1)/2))+1;
-      if(a<b)
-        min = a;
-      else
-        min = b;
-      return min;
-    }*/
-
+    static int solve(int n, int p){
+      int pages=0;
+      if(p==1 || p==n)
+        return 0;
+      else if(p<= (n/2)){
+        if(p>1)
+          pages = p/2;
+      }
+      else if(p > (n/2)){
+        if((n-p)>1)
+          pages = (n-p)/2;
+        else if (n % 2 == 0 && (n-p) == 1){
+                pages = 1;
+      }
+     
+      }
+     return pages;
+    }
     public static void main(String[] args) {
-        /*Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int p = in.nextInt();
         int result = solve(n, p);
-        System.out.println(result);*/
-    	int a = 20-15;
-    	int b = (int)Math.ceil(a/2.0);
-    	System.out.println(b);
+        System.out.println(result);
+        in.close();
     }
 }
